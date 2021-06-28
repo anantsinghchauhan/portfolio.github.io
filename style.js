80ff6ae379c459b12aa5f8fe45a4aa2f81c42db9
anantsinghchauhan <57144246+anantsinghchauhan@users.noreply.github.com>
2021-06-28 12:51:23 +0530

    update

--- style.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#menu-open").addEventListener("click", () => {
-  document.querySelector("#slidingNavbar").style.left = "0";
-});
-
-document.querySelector("#closeBtn").addEventListener("click", function () {
-  document.querySelector("#slidingNavbar").style.left = "-100%";
-});
